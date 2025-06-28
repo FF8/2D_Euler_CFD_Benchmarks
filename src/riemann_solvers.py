06abcca1f5559f983b96d34f7dc63cd8e5312b5e
Fabio Ferreira <fabio81011@gmail.com>
2025-06-28 23:07:10 +0100

    Update riemann_solvers

--- src/riemann_solvers.py
+++ src/riemann_solvers.py
@@ -22,14 +22,14 @@ def hll_flux(U_L, U_R, direction='x'):
     F_L, _ = get_flux_general(U_L_rot)
     F_R, _ = get_flux_general(U_R_rot)
     
-    c_L = np.sqrt(GAMMA * p_L / rho_L)
-    c_R = np.sqrt(GAMMA * p_R / rho_R)
+    c_L = np.sqrt(GAMMA*p_L/rho_L) # These are the sound speeds 
+    c_R = np.sqrt(GAMMA*p_R/rho_R) 
     
-    S_L = np.minimum(u_L - c_L, u_R - c_R)
-    S_R = np.maximum(u_L + c_L, u_R + c_R)
+    S_L = np.minimum(u_L-c_L, u_R-c_R) # These are the wave speeds 
+    S_R = np.maximum(u_L+c_L, u_R+c_R)
     
-    den = S_R - S_L + 1e-9
-    F_HLL = (S_R * F_L - S_L * F_R + S_L * S_R * (U_R_rot - U_L_rot)) / den
+    den = S_R-S_L+1e-9
+    F_HLL = (S_R*F_L-S_L*F_R+S_L*S_R*(U_R_rot-U_L_rot))/den
     
     flux_rot = np.where(S_L[np.newaxis, ...] >= 0, F_L, F_HLL)
     flux_rot = np.where(S_R[np.newaxis, ...] <= 0, F_R, flux_rot)
@@ -45,6 +45,7 @@ def hll_flux(U_L, U_R, direction='x'):
 
 def hllc_flux(U_L, U_R, direction='x', wave_speed_method='roe'):
     """Robust HLLC Riemann solver for the 2D Euler equations."""
+    # Rotate states for y-direction to treat it as an x-direction problem
     if direction == 'y':
         U_L_rot = np.copy(U_L); U_L_rot[1] = U_L[2]; U_L_rot[2] = U_L[1]
         U_R_rot = np.copy(U_R); U_R_rot[1] = U_R[2]; U_R_rot[2] = U_R[1]
@@ -61,7 +62,9 @@ def hllc_flux(U_L, U_R, direction='x', wave_speed_method='roe'):
         sqrt_rho_L=np.sqrt(rho_L); sqrt_rho_R=np.sqrt(rho_R)
         u_roe=(sqrt_rho_L*u_L+sqrt_rho_R*u_R)/(sqrt_rho_L+sqrt_rho_R)
         h_roe=(sqrt_rho_L*h_L+sqrt_rho_R*h_R)/(sqrt_rho_L+sqrt_rho_R)
-        v_sq_roe=(sqrt_rho_L*v_L**2+sqrt_rho_R*v_R**2)/(sqrt_rho_L+sqrt_rho_R)
+        # v_sq_roe term here would not exist in a purely 1D solver
+        # Accounts for the kinetic energy of the flow moving parallel to the cell interface.
+        v_sq_roe=(sqrt_rho_L*v_L**2+sqrt_rho_R*v_R**2)/(sqrt_rho_L+sqrt_rho_R)  
         c_roe=np.sqrt(np.maximum((GAMMA-1)*(h_roe-0.5*(u_roe**2+v_sq_roe)),1e-9))
         S_L=np.minimum(u_L-c_L, u_roe-c_roe)
         S_R=np.maximum(u_R+c_R, u_roe+c_roe)
@@ -76,7 +79,7 @@ def hllc_flux(U_L, U_R, direction='x', wave_speed_method='roe'):
 
     U_star_L=np.zeros_like(U_L_rot)
     U_star_L[0]=rho_L*(S_L-u_L)/(S_L-S_M+1e-9)
-    U_star_L[1]=U_star_L[0]*S_M; U_star_L[2]=U_star_L[0]*v_L
+    U_star_L[1]=U_star_L[0]*S_M; U_star_L[2]=U_star_L[0]*v_L # We have four terms, this is a 2D problem. 
     U_star_L[3]=U_star_L[0]*(U_L_rot[3]/rho_L+(S_M-u_L)*(S_M+p_L/(rho_L*(S_L-u_L)+1e-9)))
     
     U_star_R=np.zeros_like(U_R_rot)
@@ -96,6 +99,7 @@ def hllc_flux(U_L, U_R, direction='x', wave_speed_method='roe'):
     flux_rot=np.where(np.logical_and(np.logical_not(cond_L), np.logical_not(cond_M))[np.newaxis,...],F_star_R,flux_rot)
     flux_rot=np.where(cond_R[np.newaxis,...],F_R,flux_rot)
 
+    # De-rotate the flux if necessary
     if direction=='y':
         flux_final=np.copy(flux_rot)
         flux_final[1]=flux_rot[2]
